b8cf1a64db163db2027ee62fc82b2cf091c824c1
chiaraercolani <chiara.ercolani8@gmail.com>
2016-11-08 14:53:40 +0100

    first_commit

---
+++ app/src/main/java/com/example/chiaraercolani/treasurehunt/JoinHuntActivity.java
@@ -0,0 +1,13 @@
+package com.example.chiaraercolani.treasurehunt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JoinHuntActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join_hunt);
+    }
+}
---
+++ app/src/main/java/com/example/chiaraercolani/treasurehunt/StartMenuActivity.java
@@ -0,0 +1,39 @@
+package com.example.chiaraercolani.treasurehunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartMenuActivity extends AppCompatActivity {
+
+    private final static int CREATE_ACTIVITY=0;
+    private final static int JOIN_ACTIVITY=1;
+    private final static int SETTINGS_ACTIVITY=2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_menu);
+    }
+
+    public void buttonClicked(View view){
+        Intent intent;
+        switch(view.getId()){
+            case R.id.btn_create:
+                intent = new Intent(this,CreateHuntActivity.class);
+                startActivityForResult(intent,CREATE_ACTIVITY);
+                break;
+            case R.id.btn_join:
+                intent = new Intent(this,JoinHuntActivity.class);
+                startActivityForResult(intent,JOIN_ACTIVITY);
+                break;
+            case R.id.btn_settings:
+                intent = new Intent(this,SettingsActivity.class);
+                startActivityForResult(intent,SETTINGS_ACTIVITY);
+                break;
+        }
+
+
+    }
+}
