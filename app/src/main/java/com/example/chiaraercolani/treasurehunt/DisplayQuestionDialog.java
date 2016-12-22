bba862d0d909a148f8e87f7c883804d882e5b6d9
chiaraercolani <chiara.ercolani8@gmail.com>
2016-12-22 15:49:56 +0100

    question buttons

--- app/src/main/java/com/example/chiaraercolani/treasurehunt/DisplayQuestionDialog.java
+++ app/src/main/java/com/example/chiaraercolani/treasurehunt/DisplayQuestionDialog.java
@@ -1,6 +1,19 @@
 package com.example.chiaraercolani.treasurehunt;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 /**
  * Created by chiaraercolani on 21/12/16.
@@ -8,4 +21,97 @@ import android.app.DialogFragment;
 
 public class DisplayQuestionDialog extends DialogFragment {
 
+    TextView textView;
+    View view;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        view =inflater.inflate(R.layout.question_dialog, null);
+        builder.setView(view);
+
+        textView = (TextView)view.findViewById(R.id.question_asked);
+
+        return builder.create();
+    }
+
+    public void setStep(Step step){
+        textView.setText(step.getQuestion());
+
+        Button button1 = (Button)view.findViewById(R.id.question_dialog_button_1);
+        Button button2 = (Button)view.findViewById(R.id.question_dialog_button_2);
+        Button button3 = (Button)view.findViewById(R.id.question_dialog_button_3);
+        Button button4 = (Button)view.findViewById(R.id.question_dialog_button_4);
+
+        button1.setEnabled(true);
+        button2.setEnabled(true);
+        button3.setEnabled(true);
+        button4.setEnabled(true);
+
+        Random random = new Random(System.currentTimeMillis());
+
+        if (step.getWrongAnswer3().isEmpty()) {
+            if(step.getWrongAnswer2().isEmpty()){
+                button4.setEnabled(false);
+                button3.setEnabled(false);
+                if (random.nextInt(2) == 0) {
+                    button1.setText(step.getGoodAnswer());
+                    button2.setText(step.getWrongAnswer1());
+                }else{
+                    button2.setText(step.getGoodAnswer());
+                    button1.setText(step.getWrongAnswer1());
+                }
+            }else {
+                button4.setEnabled(false);
+                switch (random.nextInt(3)) {
+                    case  0:
+                        button1.setText(step.getGoodAnswer());
+                        button2.setText(step.getWrongAnswer1());
+                        button3.setText(step.getWrongAnswer2());
+                        break;
+                    case 1 :
+                        button2.setText(step.getGoodAnswer());
+                        button3.setText(step.getWrongAnswer1());
+                        button1.setText(step.getWrongAnswer2());
+                        break;
+                    case 2 :
+                        button3.setText(step.getGoodAnswer());
+                        button1.setText(step.getWrongAnswer1());
+                        button2.setText(step.getWrongAnswer2());
+                        break;
+                }
+            }
+        }else{
+            switch (random.nextInt(4)) {
+                case  0:
+                    button1.setText(step.getGoodAnswer());
+                    button2.setText(step.getWrongAnswer1());
+                    button3.setText(step.getWrongAnswer2());
+                    button4.setText(step.getWrongAnswer3());
+                    break;
+                case 1 :
+                    button2.setText(step.getGoodAnswer());
+                    button3.setText(step.getWrongAnswer1());
+                    button1.setText(step.getWrongAnswer3());
+                    button4.setText(step.getWrongAnswer2());
+                    break;
+                case 2 :
+                    button3.setText(step.getGoodAnswer());
+                    button1.setText(step.getWrongAnswer2());
+                    button2.setText(step.getWrongAnswer3());
+                    button4.setText(step.getWrongAnswer1());
+                    break;
+                case 3 :
+                    button4.setText(step.getGoodAnswer());
+                    button1.setText(step.getWrongAnswer2());
+                    button2.setText(step.getWrongAnswer3());
+                    button3.setText(step.getWrongAnswer1());
+                    break;
+            }
+        }
+
+
+    }
 }
