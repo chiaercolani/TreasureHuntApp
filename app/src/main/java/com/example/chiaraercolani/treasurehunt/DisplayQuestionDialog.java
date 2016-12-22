0df9eb70469c72b6b00edc8be9261c23b7e76de8
chiaraercolani <chiara.ercolani8@gmail.com>
2016-12-22 15:01:57 +0100

    fixed joinhunt path

---
+++ app/src/main/java/com/example/chiaraercolani/treasurehunt/DisplayQuestionDialog.java
@@ -0,0 +1,11 @@
+package com.example.chiaraercolani.treasurehunt;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by chiaraercolani on 21/12/16.
+ */
+
+public class DisplayQuestionDialog extends DialogFragment {
+
+}
