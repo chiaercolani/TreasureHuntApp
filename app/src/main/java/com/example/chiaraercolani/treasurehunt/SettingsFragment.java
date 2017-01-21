3171a30f64a62ddbd6ae1ec08f005f97252c4866
chiaraercolani <chiara.ercolani8@gmail.com>
2017-01-21 15:59:54 +0100

    username

--- app/src/main/java/com/example/chiaraercolani/treasurehunt/SettingsFragment.java
+++ app/src/main/java/com/example/chiaraercolani/treasurehunt/SettingsFragment.java
@@ -6,6 +6,7 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
 /**
  * Created by Vincent RICHAUD on 08/11/2016.
@@ -13,11 +14,15 @@ import android.support.v7.widget.Toolbar;
 
 public class SettingsFragment extends PreferenceFragment {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
     }
+
+
 
 //    @SuppressWarnings("deprecation")
 //    @Override
--- app/src/main/java/com/example/chiaraercolani/treasurehunt/StartMenuActivity.java
+++ app/src/main/java/com/example/chiaraercolani/treasurehunt/StartMenuActivity.java
@@ -1,9 +1,13 @@
 package com.example.chiaraercolani.treasurehunt;
 
 import android.content.Intent;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class StartMenuActivity extends AppCompatActivity {
 
@@ -11,10 +15,24 @@ public class StartMenuActivity extends AppCompatActivity {
     private final static int JOIN_ACTIVITY=1;
     private final static int SETTINGS_ACTIVITY=2;
 
+    String InputUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+        InputUser = PreferenceManager.getDefaultSharedPreferences(this).getString("pref_username",null);
+
+        TextView WelcomeMsg = (TextView) findViewById(R.id.welcome_msg_id);
+
+        if (InputUser != null || InputUser.isEmpty()){
+            WelcomeMsg.setText("Welcome "+ InputUser+ "!");
+        }else{
+            WelcomeMsg.setText("Welcome to Treasure Hunt!");
+        }
+
+
+
     }
 
     @Override
